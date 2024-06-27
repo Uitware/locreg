0301f36194c86f06437058ae6f490691968f0f7f
Perehinets Maksym <supercomf128@gmail.com>
2024-06-28 01:45:26 +0300

    Added basic auth

--- pkg/local_registry/registry.go
+++ pkg/local_registry/registry.go
@@ -1,25 +1,29 @@
 package local_registry
 
 import (
+	"archive/tar"
+	"bytes"
 	"context"
 	"fmt"
 	"github.com/docker/docker/api/types/container"
 	"github.com/docker/docker/api/types/image"
 	"github.com/docker/docker/client"
 	"github.com/docker/go-connections/nat"
+	"golang.org/x/crypto/bcrypt"
 	"io"
 	"locreg/pkg/parser"
+	"log"
+	"os"
 )
 
 // RunRegistry runs a local Docker registry container with configuration
 func RunRegistry(dockerClient *client.Client, config *parser.Config) error {
+
 	// Use configuration values
 	ctx := context.Background()
 	registryPort := fmt.Sprintf("%d", config.Registry.Port)
+	imageVersion := fmt.Sprintf("docker.io/%s:%s", config.Registry.Image, config.Registry.Tag)
 	containerPort := "5000"
-	registryVersion := config.Registry.Tag
-	registryName := config.Registry.Name
-	imageVersion := fmt.Sprintf("%s:%s", config.Registry.Image, registryVersion)
 
 	// Create specifically formatted string for port mapping
 	port, err := nat.NewPort("tcp", containerPort)
@@ -36,15 +40,13 @@ func RunRegistry(dockerClient *client.Client, config *parser.Config) error {
 	}
 
 	imagePuller, err := dockerClient.ImagePull(ctx, imageVersion, image.PullOptions{})
+	log.Default()
 	if err != nil {
+		log.Default()
 		return fmt.Errorf("failed to pull distribution image: %w", err)
 	}
-	defer func(imagePuller io.ReadCloser) {
-		err := imagePuller.Close()
-		if err != nil {
-			fmt.Printf("Failed to close image pull: %v\n", err)
-		}
-	}(imagePuller)
+	defer imagePuller.Close()
+	io.Copy(os.Stdout, imagePuller)
 
 	resp, err := dockerClient.ContainerCreate(ctx, &container.Config{
 		Image: imageVersion, // Local registry image
@@ -57,10 +59,15 @@ func RunRegistry(dockerClient *client.Client, config *parser.Config) error {
 		},
 		nil,
 		nil,
-		registryName,
+		config.Registry.Name,
 	)
 	if err != nil {
 		return fmt.Errorf("failed to create distribution container: %w", err)
+	}
+
+	err = updateConfig(dockerClient, ctx, resp.ID, config.Registry.Username, config.Registry.Password)
+	if err != nil {
+		return fmt.Errorf("failed to update config: %w", err)
 	}
 
 	err = dockerClient.ContainerStart(ctx, resp.ID, container.StartOptions{})
@@ -71,8 +78,149 @@ func RunRegistry(dockerClient *client.Client, config *parser.Config) error {
 	return nil
 }
 
+func updateConfig(
+	dockerClient *client.Client,
+	ctx context.Context,
+	containerID, username, password string,
+) error {
+	// Password configuration for registry should be hashed using bcrypt
+	//credsTarBuffer, err := prepareCreds()
+	//if err != nil {
+	//	return fmt.Errorf("fatal error: %w", err)
+	//}
+	// Config file path inside the container
+	// Prepare credentials
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		return fmt.Errorf("failed to hash password: %w", err)
+	}
+	creds := fmt.Sprintf("%s:%s\n", username, hashedPassword)
+	credsTarBuffer, err := prepareCreds(creds, "htpasswd")
+	// Docker config variables
+	containerConfigPath := "/etc/docker/registry/config.yml"
+	configUpdates := `
+auth:  
+  htpasswd:
+    realm: basic-realm    
+    path: /htpasswd
+` // May want to use structs later
+
+	//Read from config
+	reader, _, err := dockerClient.CopyFromContainer(
+		ctx,
+		containerID,
+		containerConfigPath,
+	)
+	if err != nil {
+		return fmt.Errorf("copy from container: %w", err)
+	}
+	defer reader.Close()
+
+	tarReader := tar.NewReader(reader)
+	var yamlBytes []byte
+	for {
+		header, err := tarReader.Next()
+		if err == io.EOF {
+			break // End of archive
+		}
+		if err != nil {
+			return fmt.Errorf("error reading tar file: %v", err)
+		}
+		if header.Name == "config.yml" {
+			yamlBytes, err = io.ReadAll(tarReader)
+			if err != nil {
+				return fmt.Errorf("error reading config.yml: %v", err)
+			}
+			break
+		}
+	}
+	// May use structs instead of this string manipulation
+	fmt.Println(string(yamlBytes) + configUpdates)
+	configTarBuffer, err := prepareCreds(string(yamlBytes)+configUpdates, "config.yml")
+
+	// Write the updated config back to the container
+	err = dockerClient.CopyToContainer(
+		ctx,
+		containerID,
+		"/",
+		credsTarBuffer,
+		container.CopyToContainerOptions{},
+	)
+	if err != nil {
+		return fmt.Errorf("failed to copy to container: %w", err)
+	}
+	err = dockerClient.CopyToContainer(
+		ctx,
+		containerID,
+		"/etc/docker/registry/",
+		configTarBuffer,
+		container.CopyToContainerOptions{},
+	)
+	if err != nil {
+		return fmt.Errorf("copy to container: %w", err)
+	}
+
+	return nil
+}
+
+// prepareCreds creates a tar archive with the htpasswd file data inside it stored in same way as htpasswd -Bnb command does
+func prepareCreds(fileContent, fileName string) (*bytes.Buffer, error) {
+	var buf bytes.Buffer
+	// Create a tar writer
+	tw := tar.NewWriter(&buf)
+
+	// Add a file to the tar archive
+	hdr := &tar.Header{
+		Name: fileName,
+		Mode: 0600,
+		Size: int64(len(fileContent)),
+	}
+	if err := tw.WriteHeader(hdr); err != nil {
+		return nil, fmt.Errorf("failed to write tar header: %w", err)
+	}
+	if _, err := tw.Write([]byte(fileContent)); err != nil {
+		return nil, fmt.Errorf("failed to write file content to tar: %w", err)
+	}
+	if err := tw.Close(); err != nil {
+		return nil, fmt.Errorf("failed to close tar writer: %w", err)
+	}
+	return &buf, nil
+}
+
+//func prepareCreds(username, password string) (*bytes.Buffer, error) {
+//	var buf bytes.Buffer
+//
+//	// Create a tar writer
+//	tw := tar.NewWriter(&buf)
+//
+//	// Hash the password using bcrypt
+//	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+//	if err != nil {
+//		return nil, fmt.Errorf("failed to hash password: %w", err)
+//	}
+//
+//	// Create the content similar to htpasswd -Bnb
+//	fileContent := fmt.Sprintf("%s:%s\n", username, hashedPassword)
+//
+//	// Add a file to the tar archive
+//	hdr := &tar.Header{
+//		Name: "htpasswd",
+//		Mode: 0600,
+//		Size: int64(len(fileContent)),
+//	}
+//	if err := tw.WriteHeader(hdr); err != nil {
+//		return nil, fmt.Errorf("failed to write tar header: %w", err)
+//	}
+//	if _, err := tw.Write([]byte(fileContent)); err != nil {
+//		return nil, fmt.Errorf("failed to write file content to tar: %w", err)
+//	}
+//	if err := tw.Close(); err != nil {
+//		return nil, fmt.Errorf("failed to close tar writer: %w", err)
+//	}
+//	return &buf, nil
+//}
+
 func InitCommand(configFilePath string) error {
-	// Завантаження конфігураційного файлу
 	config, err := parser.LoadConfig(configFilePath)
 	if err != nil {
 		return fmt.Errorf("failed to load config: %w", err)

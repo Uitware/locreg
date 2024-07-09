4ad370e27bcc89029378afe0c15a643eb5751064
Vitalii Stelmakh <94753413+VitaliiStelmakh@users.noreply.github.com>
2024-07-09 12:08:10 +0300

    Add loop for generate exponential backoff intervals

--- pkg/providers/azure/deploy.go
+++ pkg/providers/azure/deploy.go
@@ -17,7 +17,6 @@ import (
 	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/appservice/armappservice/v2"
 	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/resources/armresources"
 	"github.com/cenkalti/backoff/v4"
-
 )
 
 var (
@@ -28,8 +27,11 @@ var (
 	webAppsClient           *armappservice.WebAppsClient
 )
 
+// Deploy initiates the deployment of resources in Azure
 func Deploy(azureConfig *parser.Config) {
 	log.Println("☁️ Starting deployment...")
+
+	// Get the Azure subscription ID
 	subscriptionID, err := getSubscriptionID()
 	if err != nil {
 		log.Fatal(err)
@@ -38,13 +40,14 @@ func Deploy(azureConfig *parser.Config) {
 		log.Fatal("❌ AZURE_SUBSCRIPTION_ID is not set.")
 	}
 
+	// Authenticate using Azure credentials
 	cred, err := azidentity.NewDefaultAzureCredential(nil)
 	if err != nil {
 		log.Fatal(err)
 	}
 	ctx := context.Background()
 
-	// Load profile to get tunnel URL
+	// Load the user profile to get the tunnel URL
 	profilePath, err := parser.GetProfilePath()
 	if err != nil {
 		log.Fatalf("❌ Failed to get profile path: %v", err)
@@ -64,6 +67,7 @@ func Deploy(azureConfig *parser.Config) {
 		log.Fatalf("❌ Failed to validate tunnel URL: %v", err)
 	}
 
+	// Initialize Azure resource clients
 	resourcesClientFactory, err = armresources.NewClientFactory(subscriptionID, cred, nil)
 	if err != nil {
 		log.Fatal(err)
@@ -77,30 +81,35 @@ func Deploy(azureConfig *parser.Config) {
 	plansClient = appserviceClientFactory.NewPlansClient()
 	webAppsClient = appserviceClientFactory.NewWebAppsClient()
 
+	// Create a resource group
 	resourceGroup, err := createResourceGroup(ctx, azureConfig)
 	if err != nil {
 		log.Fatal(err)
 	}
 	log.Println("✅ Resource group created:", *resourceGroup.ID)
 
+	// Create an App Service plan
 	appServicePlan, err := createAppServicePlan(ctx, azureConfig)
 	if err != nil {
 		log.Fatal(err)
 	}
 	log.Println("✅ App service plan created:", *appServicePlan.ID)
 
+	// Create a Web App
 	appService, err := createWebApp(ctx, azureConfig, *appServicePlan.ID, tunnelURL)
 	if err != nil {
 		log.Fatal(err)
 	}
 	log.Println("✅ App service created:", *appService.ID)
 
+	// Write deployment information to the profile
 	err = writeProfile(azureConfig.Deploy.Provider.Azure.ResourceGroup, azureConfig.Deploy.Provider.Azure.AppServicePlan.Name, azureConfig.Deploy.Provider.Azure.AppService.Name)
 	if err != nil {
 		log.Fatalf("❌ Failed to write profile: %v", err)
 	}
 }
 
+// createResourceGroup creates a new resource group in Azure
 func createResourceGroup(ctx context.Context, azureConfig *parser.Config) (*armresources.ResourceGroup, error) {
 	log.Println("☁️ Creating Resource Group...")
 	resourceGroupResp, err := resourceGroupClient.CreateOrUpdate(
@@ -116,6 +125,7 @@ func createResourceGroup(ctx context.Context, azureConfig *parser.Config) (*armr
 	return &resourceGroupResp.ResourceGroup, nil
 }
 
+// createAppServicePlan creates a new App Service plan in Azure
 func createAppServicePlan(ctx context.Context, azureConfig *parser.Config) (*armappservice.Plan, error) {
 	log.Println("☁️ Creating App Service Plan...")
 	sku := azureConfig.Deploy.Provider.Azure.AppServicePlan.Sku
@@ -146,9 +156,8 @@ func createAppServicePlan(ctx context.Context, azureConfig *parser.Config) (*arm
 	return &resp.Plan, nil
 }
 
-
+// createWebApp creates a new Web App in Azure
 func createWebApp(ctx context.Context, azureConfig *parser.Config, appServicePlanID, tunnelURL string) (*armappservice.Site, error) {
-
 	log.Println("☁️ Creating Web App...")
 
 	siteConfig := azureConfig.Deploy.Provider.Azure.AppService.SiteConfig
@@ -164,6 +173,7 @@ func createWebApp(ctx context.Context, azureConfig *parser.Config, appServicePla
 		return nil, fmt.Errorf("❌ failed to load or create profile: %w", err)
 	}
 
+	// Set up app settings for the Docker container
 	appSettings := []*armappservice.NameValuePair{
 		{
 			Name:  to.Ptr("DOCKER_REGISTRY_SERVER_URL"),
@@ -179,6 +189,7 @@ func createWebApp(ctx context.Context, azureConfig *parser.Config, appServicePla
 		},
 	}
 
+	// Create or update the Web App
 	pollerResp, err := webAppsClient.BeginCreateOrUpdate(
 		ctx,
 		azureConfig.Deploy.Provider.Azure.ResourceGroup,
@@ -207,13 +218,16 @@ func createWebApp(ctx context.Context, azureConfig *parser.Config, appServicePla
 	return &resp.Site, nil
 }
 
+// getSubscriptionID retrieves the Azure subscription ID from the Azure CLI
 func getSubscriptionID() (string, error) {
+	// Execute the Azure CLI command to get the subscription ID
 	cmd := exec.Command("az", "account", "show", "--query", "id", "--output", "json")
 	output, err := cmd.Output()
 	if err != nil {
 		return "", err
 	}
 
+	// Parse the JSON output to extract the subscription ID
 	var result string
 	if err := json.Unmarshal(output, &result); err != nil {
 		return "", err
@@ -222,21 +236,26 @@ func getSubscriptionID() (string, error) {
 	return result, nil
 }
 
+// writeProfile updates the profile with the created Azure resources' details
 func writeProfile(resourceGroupName, appServicePlanName, appServiceName string) error {
+	// Get the profile path
 	profilePath, err := parser.GetProfilePath()
 	if err != nil {
 		return fmt.Errorf("❌ failed to get profile path: %w", err)
 	}
 
+	// Load or create a new profile
 	profile, err := parser.LoadOrCreateProfile(profilePath)
 	if err != nil {
 		return fmt.Errorf("❌ failed to load or create profile: %w", err)
 	}
 
+	// Update the profile with the new resource details
 	profile.CloudResources.ResourceGroupName = resourceGroupName
 	profile.CloudResources.AppServicePlanName = appServicePlanName
 	profile.CloudResources.AppServiceName = appServiceName
 
+	// Save the updated profile
 	err = parser.SaveProfile(profile, profilePath)
 	if err != nil {
 		return fmt.Errorf("❌ failed to save profile: %w", err)
@@ -244,7 +263,10 @@ func writeProfile(resourceGroupName, appServicePlanName, appServiceName string) 
 
 	return nil
 }
+
+// checkTunnelURLValidity checks the validity of the tunnel URL using exponential backoff
 func checkTunnelURLValidity(tunnelURL string) error {
+	// Define the operation to check the tunnel URL
 	operation := func() error {
 		// Check if tunnel URL is empty
 		if tunnelURL == "" {
@@ -252,6 +274,7 @@ func checkTunnelURLValidity(tunnelURL string) error {
 			return fmt.Errorf("tunnel URL is empty")
 		}
 
+		// Make an HTTP GET request to the tunnel URL
 		checkURL := fmt.Sprintf("https://%s", tunnelURL)
 		resp, err := http.Get(checkURL)
 		if err != nil {
@@ -260,6 +283,7 @@ func checkTunnelURLValidity(tunnelURL string) error {
 		}
 		defer resp.Body.Close()
 
+		// Check if the response status is OK
 		if resp.StatusCode != http.StatusOK {
 			log.Printf("❌ Invalid response status for URL %s: %s, retrying...", checkURL, resp.Status)
 			return fmt.Errorf("invalid response status: %s", resp.Status)
@@ -269,10 +293,17 @@ func checkTunnelURLValidity(tunnelURL string) error {
 		return nil
 	}
 
-	intervals := []time.Duration{1 * time.Second, 3 * time.Second, 7 * time.Second, 15 * time.Second, 31 * time.Second}
-	maxRetries := len(intervals)
+	// Generate exponential backoff intervals
+	maxRetries := 5
+	initialInterval := 1 * time.Second
+	intervals := make([]time.Duration, maxRetries)
+	for i := 0; i < maxRetries; i++ {
+		intervals[i] = initialInterval * (1 << i)
+	}
+
 	currentRetry := 0
 
+	// Use backoff.RetryNotify to retry the operation with exponential backoff
 	err := backoff.RetryNotify(operation, backoff.WithMaxRetries(backoff.NewConstantBackOff(1*time.Second), uint64(maxRetries)), func(err error, duration time.Duration) {
 		if currentRetry < maxRetries {
 			duration = intervals[currentRetry]

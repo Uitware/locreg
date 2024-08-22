7a33aaea6b75f61a1b3bb168c6fd328d579a8687
Maksym Perehinets <maksym.perehinets@gmail.com>
2024-08-22 18:27:41 +0300

    Enhancement: AWS provider for ECS added

--- pkg/providers/aws/deploy.go
+++ pkg/providers/aws/deploy.go
@@ -1,7 +1,20 @@
 package aws
 
-import "fmt"
+import (
+	"context"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/ecs"
+	"log"
+)
 
 func Deploy() {
-	fmt.Println("☁️ Deploying to AWS...")
+	ctx := context.Background()
+	cfg, err := config.LoadDefaultConfig(ctx)
+	if err != nil {
+		log.Fatal("configuration error, " + err.Error())
+	}
+	ecsClient := ecs.NewFromConfig(cfg)
+	ecsInstance := EcsClient{client: ecsClient}
+	subnetId := ecsInstance.deployECS(ctx, cfg)
+	ecsInstance.runService(ctx, subnetId)
 }
